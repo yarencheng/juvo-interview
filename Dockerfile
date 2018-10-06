62eb21457aa7081bb805c600d9395d15841b7a76
Aren Chen <yarencheng@gmail.com>
2018-10-06 00:24:41 -0700

    hello selenium

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM ubuntu:18.04
+
 RUN apt-get update && apt-get install -y python3 python3-pip
+
+RUN pip3 install selenium
+
 WORKDIR /workdir
 COPY main.py .
+COPY logger.py .
 ENTRYPOINT [ "python3", "main.py" ]
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print(123)
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from logger import logger
+import time
+
+def get_driver():
+    while True:
+        try:
+            driver = webdriver.Remote(
+                command_executor='http://selenium:4444/wd/hub',
+                desired_capabilities=DesiredCapabilities.CHROME
+            )
+            return driver
+        except:
+            logger.warning("Connect to selenium failed. Sleep 1 seconds and try again")
+            time.sleep(1)
+
+def main():
+    driver = get_driver()
+
+    driver.get("https://tw.buy.yahoo.com/help/helper.asp?p=sitemap")
+
+if __name__ == "__main__":
+    main()
